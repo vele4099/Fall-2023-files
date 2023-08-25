993556945b1841ecad3d46e76c949ade5aa7a23b
vele4099 <142831267+vele4099@users.noreply.github.com>
2023-08-25 18:07:38 +0000

    Classwork 8/25

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 # This is a sample python file.
 
-x = input("Enter a number: ")
-y = input("Enter another number: ")
+x = input ("Enter a number: ") 
+y = input ("Enter another number: ")
 
-print("The sum of the two numbers is: ", x+y)
+print("The sum of the two numbers is: ", x + y)
